e1a21cb88c51c7d21832a48cbde569f7f6c868ba
Jack Aitken <jaitk11@gmail.com>
2021-08-19 06:29:16 -0400

    Privatizing state

--- lesson_2/practice.js
+++ lesson_2/practice.js
@@ -1,13 +1,23 @@
-"use strict";
-
-function bind(context, func) {
-  return () => func.call(context);
+function makeList() {
+  let items = [];
+  return function(item) {
+    if (item) {
+      let index = items.indexOf(item);
+      if (index !== -1) {
+        items.splice(index, 1);
+        console.log(item);
+      } else {
+        items.push(item);
+        console.log(`${item} added!`);
+      }
+    } else {
+      if (items.length === 0) {
+        console.log('The list is empty.');
+      } else {
+        items.forEach(item => console.log(item));
+      }
+    }
+  }
 }
 
-let obj = {};
-let boundFunc = bind(obj, function() {
-  this.foo = "bar";
-});
-
-boundFunc();
-console.log(obj); // { foo: 'bar' }
+// This protects the state of makeList, but the implementation is a bit strange
