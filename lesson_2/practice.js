6022c989b896df07afc434d01a0ceeeac21956cc
Jack Aitken <jaitk11@gmail.com>
2021-08-19 06:36:02 -0400

    Privatizing state w/object

--- lesson_2/practice.js
+++ lesson_2/practice.js
@@ -20,4 +20,31 @@ function makeList() {
   }
 }
 
-// This protects the state of makeList, but the implementation is a bit strange
+function makeList() {
+  let items = [];
+  return {
+    add(item) {
+      items.push(item);
+      console.log(`${item} added`);
+    },
+
+    list() {
+      if (items.length > 0) {
+        items.forEach(item => console.log(item));
+      } else {
+        console.log('list empty');
+      }
+    },
+
+    remove(item) {
+      let index = items.indexOf(item);
+      items.splice(index, 1);
+      console.log(`${item} removed`);
+    }
+  }
+}
+
+let list = makeList();
+list.add('Milk');
+
+// we've now privatized the state and made this easier to work with  
