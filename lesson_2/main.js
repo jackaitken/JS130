6c97baa461bb8de9d6691b27a596ccb8c1bd4857
Jack Aitken <jaitk11@gmail.com>
2021-08-22 20:01:13 -0400

    throwing errors

--- lesson_2/main.js
+++ lesson_2/main.js
@@ -1,1 +1,24 @@
-console.log(Object.getPrototypeOf(ReferenceError));
+class DivideByZero extends Error {}
+
+function div(first, second) {
+  if (second === 0) {
+    throw new DivideByZero('Zero division');
+  }
+  return first / second;
+}
+
+function divideOneBy(divisor) {
+  try {
+    let result = div(1, divisor);
+    console.log(result);
+  } catch (error) {
+    if (error instanceof DivideByZero) {
+      console.log(`${error.message} ignored`);
+    } else {
+      throw error.message;
+    }
+  }
+}
+
+divideOneBy(1); // 1
+divideOneBy(0); // Zero division ignored
