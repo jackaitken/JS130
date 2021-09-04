c1de3f43769479c94be0d6b54560a0b605a28fd5
Jack Aitken <jaitk11@gmail.com>
2021-09-04 17:24:51 -0400

    stack with tests

--- lesson_5/practice.js
+++ lesson_5/practice.js
@@ -1,6 +1,27 @@
-function func() {
-  let [ ...args ] = arguments;
-  console.log(args);
+class Stack {
+  constructor() {
+    this.stack = [];
+  }
+
+  push(num) {
+    this.stack.push(num);
+  }
+
+  pop() {
+    return this.stack.pop();
+  }
+
+  isEmpty() {
+    return this.stack.length === 0;
+  }
+
+  peek() {
+    return this.stack[this.stack.length - 1] || 'Stack is empty';
+  }
+
+  size() {
+    return this.stack.length;
+  }
 }
 
-func(1, 2, 3);
+module.exports = Stack;
---
+++ lesson_5/practice.test.js
@@ -0,0 +1,45 @@
+let Stack = require('./practice.js');
+
+describe('Testing stack operations', () => {
+  let stack;
+
+  beforeEach(() => {
+    stack = new Stack();
+  });
+
+  test('Empty stack', () => {
+    expect(stack.isEmpty()).toBeTruthy();
+  });
+
+  test('Pushing element to stack', () => {
+    stack.push(10);
+    expect(stack.isEmpty()).toBeFalsy();
+    expect(stack.peek()).toEqual(10);
+    expect(stack.size()).toEqual(1);
+  });
+
+  test('Popping element from stack', () => {
+    stack.push(1);
+    stack.push(2);
+    let elem = stack.pop();
+    expect(elem).toEqual(2);
+    expect(stack.size()).toEqual(1);
+  });
+
+  test('Peek returns most recent pushed element', () => {
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+    expect(stack.peek()).toEqual(3);
+    expect(stack.size()).toEqual(3);
+  });
+
+  test('Size returns actual size of stack', () => {
+    for (let i = 0; i < 25; i++) {
+      stack.push(i);
+    }
+
+    expect(stack.size()).toEqual(25);
+    expect(stack.peek()).toEqual(24);
+  });
+});
